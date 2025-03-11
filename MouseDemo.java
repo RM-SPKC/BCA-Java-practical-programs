9808c95ee32cd79143246e6491258839cc0da3b0
RM <rm.spkcit@gmail.com>
2025-03-11 23:32:19 +0530

    Add files via upload

---
+++ MouseDemo.java
@@ -0,0 +1,49 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+class MouseDemo extends MouseAdapter
+{
+  JLabel l;
+  JFrame f;
+  Font fo;
+  
+  MouseDemo()
+  {
+    f=new JFrame();
+	f.setSize(600,600);
+	f.setVisible(true);
+	l=new JLabel("",JLabel.CENTER);
+	f.add(l);
+	fo=new Font("Arial",Font.BOLD,32);
+	l.setFont(fo);
+	f.addMouseListener(this);
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+  
+  public void mouseEntered(MouseEvent m)
+  {
+    l.setText("Mouse Entered");
+  }
+  public void mouseExited(MouseEvent m)
+  {
+    l.setText("Mouse Exited");
+  }
+  public void mouseClicked(MouseEvent m)
+  {
+    l.setText("Mouse Clicked");
+  }
+  public void mousePressed(MouseEvent m)
+  {
+    l.setText("Mouse Pressed");
+  }
+  public void mouseReleased(MouseEvent m)
+  {
+    l.setText("Mouse Released");
+  }
+  
+  public static void main(String args[])
+  {
+   new MouseDemo();  
+  }
+  
+}
